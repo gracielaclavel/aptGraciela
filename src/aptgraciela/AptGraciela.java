aa836b970c5130234db8ec15652ca29f6aedcf41
gracielaclavel <gracy63@gmail.com>
2017-10-25 21:11:38 -0300

    Trabajo del ejercios 3

---
+++ src/aptgraciela/AptGraciela.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aptgraciela;
+
+/**
+ *
+ * @author lp12
+ */
+public class AptGraciela {
+
+      
+      
+    
+    
+    public static void main(String[] args){
+     
+       int numero;//Declaracion de variable tipo entero
+      double numero2;//Declaracion variable tipo double
+      
+       numero=5;//Acabo de inicalizar la variable numero
+      numero2=3;
+    
+     System.out.println ("Suma de numero y numero2: "+(numero+numero2));
+     System.out.println ("Resta de numero y numero2: "+(numero-numero2));  
+     System.out.println ("Multiplicacion de numero y numero2: "+(numero*numero2));         
+     System.out.println ("Division de numero y numero2: "+(numero/numero2));
+      System.out.println ("Residuo de numero y numero2: "+(numero%numero2));
+    
+    
+    
+        // TODO code application logic here
+    }
+    
+}
